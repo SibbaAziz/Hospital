358dc14a3586ea4b0728f1e0e8db53804e2ba9eb
Aziz Sibba <aziz.sibba@adias.fr>
2019-09-18 18:03:02 +0200

    IconButton control

---
+++ Hospital.Wpf/Controls/IconButton.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Hospital.Wpf.Controls
+{
+    public class IconButton : Button
+    {
+        static IconButton()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(IconButton), new FrameworkPropertyMetadata(typeof(IconButton)));
+        }
+
+
+
+        public object Icon
+        {
+            get { return (object)GetValue(IconProperty); }
+            set { SetValue(IconProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for MyProperty.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty IconProperty =
+            DependencyProperty.Register("Icon", typeof(object), typeof(IconButton), new PropertyMetadata(null));
+
+
+    }
+}
